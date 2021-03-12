716ff430dbb94e6b897d3d0416145b74e78a6b86
Philipp Christ <philipp.christ.1997@gmail.com>
2021-03-12 15:20:08 +0100

    feat: Adding functions and resources to serverless.yml
    Adding CRUD functions

--- backend/src/lambda/http/getImages.ts
+++ backend/src/lambda/http/getImages.ts
@@ -1,17 +1,40 @@
 import 'source-map-support/register'
+
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import { getImages } from '../../businessLayer/images'
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+import { getHeaders } from '../utils'
+import { createLogger } from '../../utils/logger';
+
+const XAWS = AWSXRay.captureAWS(AWS)
+const docClient = new XAWS.DynamoDB.DocumentClient()
+const imagesTable = process.env.IMAGES_TABLE;
+const indexName = process.env.INDEX_UI_CD_NAME;
+
+const logger = createLogger('image-get-list')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  
-  // TODO: Get all TODO items for a current user
+  const userId = event.requestContext.authorizer.principalId;
+
+  const result = await docClient.query({
+    TableName: imagesTable,
+    IndexName: indexName,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
+  }).promise();
+
+  const items = result.Items;
+
+  logger.info("get images success")
+
   return {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
+    headers: getHeaders(),
     body: JSON.stringify({
-      items: await getImages(event)
+      items
     })
-  };
+  }
 }
